740c7d35280ac4b9ab748e4d985da94fa3ea62e2
nishant0524 <nishant.18184@sakec.ac.in>
2026-04-26 12:44:23 +0530

    ec2 checked

---
+++ tests/fixtures/ec2.tf
@@ -0,0 +1,75 @@
+# key pair 
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "terra-key-ec2"
+  public_key = file("terra-key-ec2.pub")
+}
+
+#VPC and Security groups
+
+resource "aws_default_vpc" "my_vpc" {
+
+}
+
+resource "aws_security_group" "terra_sg" {
+  name   = "terra-sg"
+  vpc_id = aws_default_vpc.my_vpc.id
+
+  #inbound rules
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.ip_address}"]
+    description = "SSH port open"
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.ip_address}"]
+    description = "HTTP port open"
+  }
+
+  # outbound rules
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow Https outbound traffic"
+  }
+
+  tags = {
+    Name = "terra-sg"
+  }
+}
+
+# EC2 instance
+
+resource "aws_instance" "terra_instance" {
+  # count           = 3   #here 3 instance will be created of same type and same name
+  for_each = tomap({
+    automate-terra-instance-micro = "t3.micro"
+    automate-terra-instance-small = "t3.small"
+  })
+
+  depends_on      = [aws_security_group.terra_sg, aws_key_pair.my_key]
+  key_name        = aws_key_pair.my_key.key_name
+  security_groups = [aws_security_group.terra_sg.name]
+  instance_type   = each.value
+  ami             = var.ec2_ami_id
+  user_data       = file("install_nginx.sh")
+
+
+  root_block_device {
+    volume_size = var.env == "prod" ? 20 : var.ec2_default_volume_size
+    volume_type = var.ec2_volume_type
+  }
+
+  tags = {
+    Name = each.key
+  }
+}
